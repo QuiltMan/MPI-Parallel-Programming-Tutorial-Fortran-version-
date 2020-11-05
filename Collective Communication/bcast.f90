ef4464c87f49b0a79eca82a2984ad6614c747c95
Pengcheng Chen <34759341+QuiltMan@users.noreply.github.com>
2020-11-05 19:52:04 +0800

    Update bcast.f90

--- Collective Communication/bcast.f90
+++ Collective Communication/bcast.f90
@@ -1,1 +1,35 @@
+program ssendrcv
+	use mpi
 
+	implicit none
+
+	
+	integer rank, value, size, ierr
+	integer status(MPI_STATUS_SIZE);
+	integer,allocatable,dimension(:) :: buffer
+	integer,parameter :: buf_size = 10
+	
+	allocate(buffer(buf_size))
+	
+	call MPI_INIT(ierr)
+	call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierr)
+	call MPI_COMM_SIZE(MPI_COMM_WORLD, size, ierr)
+	
+	do while(value .ge. 0)
+		if(rank == 0) then
+			print *, "Please enter a value, -1 means finish"		
+			read *,value 
+			call MPI_BCAST(value,1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
+		else 
+			call MPI_BCAST(value,1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
+			write (*,100) rank, value
+		end if
+	enddo
+	
+	call MPI_FINALIZE(ierr)
+	deallocate(buffer)
+	
+	100 FORMAT ('Process', I6, ' got ', I6 )
+	
+	stop
+end program ssendrcv
